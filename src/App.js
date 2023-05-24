17c02769672dae885c7b78b534230f98b0f3d574
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-05-24 11:32:23 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { TodoList } from './Todo.js';
+
+const initialTodoItems = [
+    { id: 0, description: "Go shopping to the Mall", status: false, deleted: false },
+    { id: 1, description: "Go for a walk in the morning", status: true, deleted: false },
+    { id: 2, description: "Learn React.js basics", status: false, deleted: false },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todoItems, setTodoItems] = useState(initialTodoItems);
+    const [showDeletedTodos, setShowDeletedTodos] = useState(false);
+    const [createTodoInputText, setCreateTodoInputText] = useState("");
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setTodoItems([...todoItems, {
+            id: Math.random(),
+            description: createTodoInputText,
+            status: false,
+            deleted: false
+        }]);
+    }
+
+    const updateTodo = (id, data) => {
+        const newTodoItems = [...todoItems];
+        for(const todoItem of newTodoItems) {
+            if(todoItem.id == id) {
+                for(const key of Object.keys(data)) {
+                    if(todoItem.hasOwnProperty(key)) {
+                        todoItem[key] = data[key];
+                    }
+                }
+            }
+        }
+
+        setTodoItems(newTodoItems);
+    }
+
+    const deletedTodoFilter = (todo) => todo.deleted == true;
+
+    return (
+        <div className="App">
+        <h2>Todo List</h2>
+        <TodoList todoList={todoItems} updateTodo={updateTodo} />
+        <h2>Deleted</h2>
+        {showDeletedTodos ?
+            <div>
+                <button onClick={() => setShowDeletedTodos(false)}>Hide Deleted</button>
+                <TodoList todoList={todoItems} updateTodo={updateTodo} filter={deletedTodoFilter} />
+            </div> :
+            <button onClick={() => setShowDeletedTodos(true)}>Show Deleted</button>
+        }
+        <h2>Create Todo</h2>
+        <form onSubmit={onSubmit} >
+        <input placeholder="Type todo here..." type="text" width={ 20 } height={ 20 } onChange={(e) => setCreateTodoInputText(e.target.value)} />
+        <input type="submit" value="Add" />
+        </form>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+
+const EDIT_MODE = 1;
+const NORMAL_MODE = 2;
+
+/**
+ * Item:
+ *  - description
+ *  - status: pending/completed
+ *  - deleted 
+ *  - category
+ *  - tags
+ */
+
+export function Todo({ todo, updateTodo } ) {
+    const [mode, setMode] = useState(NORMAL_MODE);
+    const [editModeInputText, setEditModeInputText] = useState("");
+
+    const handleNormalModeInputChange = () => {
+        updateTodo(todo.id, { status: !todo.status });
+    }
+
+    const onEdit = () => {
+        setMode(EDIT_MODE);
+        setEditModeInputText(todo.description);
+    }
+
+    const onDelete = () => {
+        updateTodo(todo.id, { deleted: true });
+    }
+
+    const onSave = () => {
+        setMode(NORMAL_MODE);
+        updateTodo(todo.id, { description: editModeInputText });
+    }
+
+    const onCancel = () => {
+        setMode(NORMAL_MODE);
+        setEditModeInputText(todo.description);
+    }
+
+    const onRestore = () => {
+        updateTodo(todo.id, { deleted: false });
+    }
+
+    const handleEditModeInputChange = (e) => {
+        setEditModeInputText(e.target.value);
+    }
+
+    if(todo.deleted == true) {
+        return (
+            <div>
+                <input type="checkbox" checked={todo.status} onChange={handleNormalModeInputChange} />
+                <span>{todo.description}</span>
+                <button onClick={onRestore}>Restore</button>
+            </div>
+        );
+    }
+
+    if(mode == NORMAL_MODE) {
+        return (
+            <div>
+            <input type="checkbox" checked={todo.status} onChange={handleNormalModeInputChange} />
+            <span>{todo.description}&nbsp;</span>
+            <button onClick={onEdit}>Edit</button>
+            <button onClick={onDelete}>Delete</button>
+            </div>
+        ) 
+    }
+
+    else if(mode == EDIT_MODE)
+    {
+        return <div>
+            <input type="text" value={editModeInputText} onChange={handleEditModeInputChange} />
+            <button onClick={onSave}>Save</button>
+            <button onClick={onCancel}>Cancel</button>
+            </div>
+    }
+
+    return "";
+}
+
+export function TodoList({ todoList, updateTodo, filter }) {
+    return <ul>{todoList.filter(filter ? todo => filter(todo) : todo => todo.deleted == false).map(todo => 
+        <li>
+            <Todo key={todo.id} todo={todo} updateTodo={updateTodo} />
+        </li>
+    )}</ul>
+}
